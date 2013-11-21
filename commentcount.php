5af082081f3db0fe5212f027ce68e81d9d5b1579
karlward <karl.ward@nyu.edu>
2013-11-21 15:01:06 -0500

    new template for comment count --KW

---
+++ commentcount.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Comment Count
+ * The template for displaying comment count for all users.
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+<p>This is a word count of each student's written contribution to the class blog.</p>
+<ul>
+  <?php 
+
+  ?>
+</ul>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
